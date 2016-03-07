09168bd7445fcb60b228a2880a6714c93f3810a9
Kane Rogers <kane.m.rogers@gmail.com>
2016-03-07 18:11:02 +1100

    Add buildslave for NDK 16.

--- slaves/android/install-ndk.sh
+++ slaves/android/install-ndk.sh
@@ -8,6 +8,13 @@ set -ex
 curl -O http://dl.google.com/android/ndk/android-ndk-r10e-linux-x86_64.bin
 chmod +x android-ndk-r10e-linux-x86_64.bin
 ./android-ndk-r10e-linux-x86_64.bin > /dev/null
+
+bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
+        --platform=android-16 \
+        --toolchain=arm-linux-androideabi-4.8 \
+        --install-dir=/android/ndk-arm-16 \
+        --ndk-dir=/android/android-ndk-r10e \
+        --arch=arm
 bash android-ndk-r10e/build/tools/make-standalone-toolchain.sh \
         --platform=android-18 \
         --toolchain=arm-linux-androideabi-4.8 \
